a7dea69c97a54c2b4fe6bbb877d15d5607d4ea55
Dasha <darjaazarova@gmail.com>
2022-04-18 16:27:20 +0300

    Added the task 1

--- src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
+++ src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
@@ -2,6 +2,16 @@ package ru.gb.shurupova.homework2;
 
 public class HomeWorkTwo {
     public static void main(String[] args) {
+        checkSum(1, 1);
 
+    }
+
+    public static int checkSum(int a, int b) {
+        if ((a + b) > 10 && (a + b) <= 20) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        return a + b;
     }
 }
