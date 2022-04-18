9c721bf684e977648ca3a6daf0b1e23aa7fb6a5d
Dasha <darjaazarova@gmail.com>
2022-04-18 17:38:55 +0300

    task 1 - another solution

--- src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
+++ src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
@@ -2,16 +2,14 @@ package ru.gb.shurupova.homework2;
 
 public class HomeWorkTwo {
     public static void main(String[] args) {
-        checkSum(1, 1);
-
+        System.out.println(checkSum(10, 1));
     }
 
-    public static int checkSum(int a, int b) {
+    public static boolean checkSum(int a, int b) {
         if ((a + b) > 10 && (a + b) <= 20) {
-            System.out.println("true");
+            return true;
         } else {
-            System.out.println("false");
+            return false;
         }
-        return a + b;
     }
 }
