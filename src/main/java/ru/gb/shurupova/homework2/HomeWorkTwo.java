ecbb5770f023ad1597d78a5b4d2e320b36a3bbf0
Dasha <darjaazarova@gmail.com>
2022-04-18 18:32:36 +0300

    task 3 - solution

--- src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
+++ src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
@@ -3,9 +3,19 @@ package ru.gb.shurupova.homework2;
 public class HomeWorkTwo {
     public static void main(String[] args) {
 //        System.out.println(checkSum(19, 1));
-        numberType(0);
+//        numberType(0);
+        System.out.println(checkNumber(-1));
 
     }
+
+
+    public static boolean checkNumber(int a) {
+        if (a < 0) {
+            return true;
+        }
+        return false;
+    }
+
 
     public static void numberType(int a) {
         if (a >= 0) {
