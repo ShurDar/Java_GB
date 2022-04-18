91136b4517e2ba382d8e3cc5566f2579dff64f9a
Dasha <darjaazarova@gmail.com>
2022-04-18 17:55:13 +0300

    task 2 - solution

--- src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
+++ src/main/java/ru/gb/shurupova/homework2/HomeWorkTwo.java
@@ -2,7 +2,18 @@ package ru.gb.shurupova.homework2;
 
 public class HomeWorkTwo {
     public static void main(String[] args) {
-        System.out.println(checkSum(10, 1));
+//        System.out.println(checkSum(19, 1));
+        numberType(-8);
+
+    }
+
+    public static int numberType(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
+        }
+        return a;
     }
 
     public static boolean checkSum(int a, int b) {
