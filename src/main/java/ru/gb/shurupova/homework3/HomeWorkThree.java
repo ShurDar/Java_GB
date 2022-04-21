d5122e23a322a22040a6c92e7d44025a01e655cf
Dasha <darjaazarova@gmail.com>
2022-04-21 17:17:53 +0300

    task 1

--- src/main/java/ru/gb/shurupova/homework3/HomeWorkThree.java
+++ src/main/java/ru/gb/shurupova/homework3/HomeWorkThree.java
@@ -1,4 +1,26 @@
 package ru.gb.shurupova.homework3;
 
 public class HomeWorkThree {
+    public static void main(String[] args) {
+
+        int[] b = new int[5];
+        b[0] = 1;
+        b[1] = 1;
+        b[2] = 0;
+        b[3] = 1;
+        b[4] = 0;
+
+        for (int y = 0; y < b.length; y++) {
+            System.out.print(b[y] + " -> ");
+
+            if (b[y] == 0) {
+                b[y] = b[y] + 1;
+            } else {
+                b[y] = b[y] - 1;
+            }
+
+            System.out.print(b[y]);
+            System.out.println();
+        }
+    }
 }
