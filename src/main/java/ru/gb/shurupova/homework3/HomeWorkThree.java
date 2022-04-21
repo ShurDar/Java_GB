9493abba52d6ba2999f71c14b3f142c5c3779c5d
Dasha <darjaazarova@gmail.com>
2022-04-21 20:24:37 +0300

    task 3

--- src/main/java/ru/gb/shurupova/homework3/HomeWorkThree.java
+++ src/main/java/ru/gb/shurupova/homework3/HomeWorkThree.java
@@ -4,6 +4,20 @@ public class HomeWorkThree {
     public static void main(String[] args) {
 
 
+        // task 3
+        int[] c = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int z = 0; z < c.length; z++) {
+            System.out.print(c[z] + " -> ");
+            if (c[z] < 6) {
+                c[z] = c[z] * 2;
+            } else {
+                c[z] = c[z];
+            }
+            System.out.print(c[z]);
+            System.out.println();
+        }
+
+
         // task 2
         int[] a = new int[100];
         for (int x = 0; x < a.length; x++) {
